d61d28eca7cf1c429fb7afabe3e89af4e5b4f13e
ConfiacB <61695639+ConfiacB@users.noreply.github.com>
2020-04-14 21:59:02 +0200

    day 1, step1-07

---
+++ step1-07/TodoFooter.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Todos } from '../TodoApp.types';
+
+interface TodoFooterProps {
+  clear: () => void;
+  todos: Todos;
+}
+
+export const TodoFooter = (props: TodoFooterProps) => {
+  const itemCount = Object.keys(props.todos).filter(id => !props.todos[id].completed).length;
+  const _onClick = () => {
+    props.clear();
+  }
+
+  return (
+    <footer>
+      <span>
+        {itemCount} item{itemCount === 1 ? '' : 's'} left
+      </span>
+      <button className="submit" onClick={_onClick}>Clear Completed</button>
+    </footer>
+  );
+};
