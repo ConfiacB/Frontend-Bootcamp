eaa90e086406dd18f390737e53a0c1d500675890
ConfiacB <61695639+ConfiacB@users.noreply.github.com>
2020-04-15 12:25:44 +0200

    day 2, step 4

---
+++ step2-04/components/TodoFooter.tsx
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react';
+import { DefaultButton, Stack, Text } from 'office-ui-fabric-react';
+import { TodoContext } from '../TodoContext';
+
+export const TodoFooter = () => {
+  // TODO: replace the following with a useContext(TodoContext) calls
+  const context = useContext(TodoContext);
+  // - end of exercise for this file -
+
+  const itemCount = Object.keys(context.todos).filter(id => !context.todos[id].completed).length;
+
+  return (
+    <Stack horizontal horizontalAlign="space-between" verticalAlign="center">
+      <Text>
+        {itemCount} item{itemCount === 1 ? '' : 's'} left
+      </Text>
+      <DefaultButton onClick={() => context.clear()}>Clear Completed</DefaultButton>
+    </Stack>
+  );
+};
