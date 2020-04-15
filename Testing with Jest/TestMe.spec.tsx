52e71a667dcc5e9032b5624f6f42c57c3d0ee022
ConfiacB <61695639+ConfiacB@users.noreply.github.com>
2020-04-15 13:05:47 +0200

    bonus test TestMe

---
+++ Testing with Jest/TestMe.spec.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { TestMe } from './TestMe';
+
+
+describe('TestMe Component', () => {
+  it('should render correctly when hovered', () => {
+    // TODO:
+    // 1. mount a <TestMe> Component here
+    // 2. use enzyme wrapper's find() method to retrieve the #innerMe element
+    // 3. simulate a hover with "mouseover" event via the simulate() API
+    // 4. make assertions with expect on the text() of the #innerMe element
+    const wrapper = mount(<TestMe name="world" />);
+    wrapper.find('#innerMe').simulate('mouseover');
+    expect(wrapper.find('#innerMe').text()).toBe('Enabled');
+  });
+});
